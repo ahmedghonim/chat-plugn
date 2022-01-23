851f9c4954f33f2775923b5b6494d22de55e5ae9
Ahmed <ahmed.ghonim.dev@gmail.com>
2022-01-23 11:52:57 +0200

    fix

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -3,27 +3,19 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import "./styles.scss";
 const NavBar = (props) => {
-
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <NavLink to="/">
-              <Nav.Link href="/">الرئيسية</Nav.Link>
-            </NavLink>
-            <NavLink to="/about-us">
-              <Nav.Link href="/about-us">من نحن</Nav.Link>
-            </NavLink>
-            <NavLink to="/chat">
-              <Nav.Link href="/chat">المحادثة</Nav.Link>
-            </NavLink>
+            <NavLink to="/" className="nav-item nav-link">الرئيسية</NavLink>
+            <NavLink to="/about-us" className="nav-item nav-link">من نحن</NavLink>
+            <NavLink to="/chat" className="nav-item nav-link">المحادثة</NavLink>
           </Nav>
         </Navbar.Collapse>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Brand href="/home">الشعار</Navbar.Brand>
       </Container>
-    
     </Navbar>
   );
 };
